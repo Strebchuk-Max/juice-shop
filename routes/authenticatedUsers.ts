df1a574228d65f318e62c04900977d90038767fa
Maxim Strebchuk <strebchuk.m.i@gmail.com>
2024-10-30 12:37:43 +0200

    Fix Privacy Violation

--- routes/authenticatedUsers.ts
+++ routes/authenticatedUsers.ts
@@ -7,31 +7,32 @@ import { UserModel } from '../models/user'
 import { decode } from 'jsonwebtoken'
 import * as security from '../lib/insecurity'
 
-async function retrieveUserList (req: Request, res: Response, next: NextFunction) {
-  try {
-    const users = await UserModel.findAll()
+async function retrieveUserList(req: Request, res: Response, next: NextFunction) {
+    try {
+        const users = await UserModel.findAll()
 
-    res.json({
-      status: 'success',
-      data: users.map((user) => {
-        const userToken = security.authenticatedUsers.tokenOf(user)
-        let lastLoginTime: number | null = null
-        if (userToken) {
-          const parsedToken = decode(userToken, { json: true })
-          lastLoginTime = parsedToken ? Math.floor(new Date(parsedToken?.iat ?? 0 * 1000).getTime()) : null
-        }
+        res.json({
+            status: 'success',
+            data: users.map((user) => {
+                const userToken = security.authenticatedUsers.tokenOf(user)
+                let lastLoginTime: number | null = null
+                if (userToken) {
+                    const parsedToken = decode(userToken, { json: true })
+                    lastLoginTime = parsedToken ? Math.floor(new Date((parsedToken?.iat ?? 0) * 1000).getTime()) : null
+                }
 
-        return {
-          ...user.dataValues,
-          password: user.password?.replace(/./g, '*'),
-          totpSecret: user.totpSecret?.replace(/./g, '*'),
-          lastLoginTime
-        }
-      })
-    })
-  } catch (error) {
-    next(error)
-  }
+                // Mask sensitive data such as password and TOTP secret
+                return {
+                    ...user.dataValues,
+                    password: '*****', // Hide the password entirely, replacing it with a placeholder
+                    totpSecret: '*****', // Replace with a placeholder for enhanced security
+                    lastLoginTime
+                }
+            })
+        })
+    } catch (error) {
+        next(error)
+    }
 }
 
 export default () => retrieveUserList
